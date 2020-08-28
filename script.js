9a526d573ba32abc27aff2832322fcfca4d77bd5
pulkit-jasti <31801730+pulkit-jasti@users.noreply.github.com>
2020-08-28 20:41:33 +0530

    styles update

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('switch').addEventListener('input', () => {
+	console.log(document.getElementById('switch').value);
+});
+
+document.getElementById('length').addEventListener('input', e => {
+	console.log(document.getElementById('length').value);
+});
